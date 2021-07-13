2d038be0bbc6ed3053c0f289c621d35a7617234c
Uma Shree <umashree31jan@gmail.com>
2021-07-13 16:10:14 +0530

    Update IPDemo.java

--- IPDemo.java
+++ IPDemo.java
@@ -5,7 +5,7 @@ public class IPDemo
 {
 public static void main(String ar[])throws Exception
 {
-InetAddress []ia=InetAddress.getAllByName("www.melsorg.in");
+InetAddress []ia=InetAddress.getAllByName("www.google.co.in");
 for(int i=0;i<ia.length;i++)
 {
 System.out.println("Local IPAddress:"+ia[i].getHostAddress());
